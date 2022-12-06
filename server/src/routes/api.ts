a673c8e7de99f0520f88465d3bd219152119d391
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2022-12-06 01:37:54 -0500

    remove JSON req for op

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -51,10 +51,10 @@ const router = Router()
 
 export const op = (req: Request<Event>, res: Response) => {
     // we expect a json body
-    if (!req.is('application/json')) {
-        console.log("Not json")
-        return res.json({ error: true, message: "Not json" })
-    }
+    // if (!req.is('application/json')) {
+    //     console.log("Not json")
+    //     return res.json({ error: true, message: "Not json" })
+    // }
 
     //console.log("OP CALLED")
 
